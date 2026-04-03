9e6ee64c55e3441832084296152685325b41bb8b
闲 <56146888+byilrq@users.noreply.github.com>
2026-04-03 09:13:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "e345ede60e6e")
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://asset_user:by123@localhost/asset_manager"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "/mnt/koofr_webdav/Koofr/asset_manager_images"
+    UPLOAD_FOLDER = "/mnt/openlist_dav/asset_manager_images"
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024
